2d6e8b5e15e12ca0b87d46ce53e90fb854f470c5
marcoVinha <marvsdd.01@live.com>
2024-12-27 20:17:13 -0300

    better print when running hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
+from rich.console import Console
+from rich.markdown import Markdown
+
+
 def main():
+    console = Console()
+
     with open("README.md") as readme_file:
-        print(readme_file.read())
+        md = Markdown(readme_file.read())
+        console.print(md)
+
 
 if __name__ == "__main__":
     main()
